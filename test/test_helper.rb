07e852f3fc08d61a0beda80be5522da026f1d6ca
Zach Ahn <engineering+handy@zachahn.com>
2016-04-13 16:53:39 -0400

    Add some test helpers for to create git repos

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,4 +6,36 @@ require_relative "support/declarative"
 
 class TestCase < Minitest::Test
   extend Support::Declarative
+
+  def sandboxes_path
+    File.expand_path("sandboxes", File.dirname(__FILE__))
+  end
+
+  def sandbox_path
+    @sandbox_path ||= File.join(sandboxes_path, "mugatu_test_#{random_string}")
+  end
+
+  def random_string(length = 8)
+    ("a".."z")
+      .to_a
+      .shuffle
+      .slice(0, length)
+      .join
+  end
+
+  def git_init
+    if File.exist?(sandbox_path)
+      raise "can't run tests, sandbox already exists"
+    end
+
+    Dir.mkdir(sandbox_path)
+
+    Dir.chdir(sandbox_path) do
+      Cocaine::CommandLine.new("git", "init").run
+    end
+  end
+
+  def sandbox_clean
+    FileUtils.rm_rf(sandbox_path)
+  end
 end
