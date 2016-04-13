b0ef1bccf0abdf683dd150ebb164bde7f8c741f0
Zach Ahn <engineering+handy@zachahn.com>
2016-04-13 11:05:38 -0400

    add support for declarative tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,5 +2,8 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'mugatu'
 
 require 'minitest/autorun'
+require_relative "support/declarative"
+
 class TestCase < Minitest::Test
+  extend Support::Declarative
 end
