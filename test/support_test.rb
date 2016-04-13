4210f1b97ace5ab78f44cbb9766cf2d64acf2d09
Zach Ahn <engineering+handy@zachahn.com>
2016-04-13 18:08:44 -0400

    Test test helpers

---
+++ test/support_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class SupportTest < TestCase
+  test "integration of test helpers" do
+    git_init
+    assert File.exist?(File.join(sandbox_path, ".git"))
+
+    touch("new.rb", %(puts "new"\n))
+    assert File.exist?(File.join(sandbox_path, "new.rb"))
+
+    git_commit_all "crazy commit message"
+
+    rm("new.rb")
+    refute File.exist?(File.join(sandbox_path, "new.rb"))
+
+    git_commit_all
+
+    out, err = capture_io { git_status }
+    assert_match %r{nothing to commit, working directory clean}, err
+
+    out, err = capture_io { git_log }
+    assert_match %r{crazy commit message}, err
+
+    sandbox_clean
+    refute File.exist?(File.join(sandbox_path))
+  end
+end
