14754d00151adf4a698ade0824c43a7d3f0c23eb
Zach Ahn <engineering+handy@zachahn.com>
2016-05-31 11:41:32 +0900

    Make CenterForAnts act enumerable

--- lib/mugatu/center_for_ants.rb
+++ lib/mugatu/center_for_ants.rb
@@ -2,21 +2,24 @@ module Mugatu
   class CenterForAnts
     include Enumerable
 
-    def initialize(driver:, root:)
+    def initialize(driver:, root:, files: [])
       @driver = driver
       @root   = root
+      @files  = files
     end
 
     def each
       if !block_given?
         return enum_for(:each)
       end
+
+      call.each(&Proc.new)
     end
 
-    def call(changed_files)
+    def call(changed_files = nil)
       runner = @driver::Runner.new(root: @root)
 
-      result = runner.call(changed_files)
+      result = runner.call(changed_files || @files)
       result_hash = normalize_to_hash(result)
 
       parser = @driver::Parser.new
---
+++ test/center_for_ants_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+
+class CenterForAntsTest < TestCase
+  test "#call" do
+    cfa = center_for_ants
+
+    assert_equal(3, cfa.call.length)
+    assert_kind_of(Mugatu::Problem, cfa.call[0])
+  end
+
+  test "#each without a block returns an Enumerator of problems" do
+    assert_kind_of(Enumerable, center_for_ants.each)
+  end
+
+  test "#each accepts a block" do
+    run_count = 0
+    center_for_ants.each do |problem|
+      assert_kind_of(Mugatu::Problem, problem)
+      run_count += 1
+    end
+
+    assert_equal(3, run_count)
+  end
+
+  private
+
+  def center_for_ants
+    Mugatu::CenterForAnts.new(
+      driver: Mugatu::Drivers::RubocopDriver,
+      root: File.dirname(__FILE__),
+      files: %w(fixtures/horrible_file.rb)
+    )
+  end
+end
