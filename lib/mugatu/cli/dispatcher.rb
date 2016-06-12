dade70e91b2d7e4467b8b8e91e67c1549d2d7051
Zach Ahn <engineering+handy@zachahn.com>
2016-06-13 02:51:04 +0900

    Heavily simplify Cli::Dispatcher

--- lib/mugatu/cli/dispatcher.rb
+++ lib/mugatu/cli/dispatcher.rb
@@ -1,36 +1,29 @@
 module Mugatu
   module Cli
-    class Dispatcher < Thor
-      include Thor::Actions
+    class Dispatcher
+      def self.start(options, paths)
+        instance = new(options)
+        instance.lint(*paths)
+      end
 
-      desc "lint", "Run linters"
-      method_option :ref, desc: "Root for diff"
+      def initialize(options)
+        @options = options
+      end
+
+      attr_reader :options
+
       def lint(*paths)
         Commands::Lint.new(bootloader, paths, options)
-      end
-
-      desc "init PATH", "Create an example .mugatu.yml at PATH"
-      def init(path)
-        puts "not yet implemented"
-        puts "example:"
-        puts File.read(File.expand_path("../../../.mugatu.yml", File.dirname(__FILE__)))
-      end
-
-      desc "version", "Print version"
-      def version
-        puts "mugatu #{Mugatu::VERSION}"
-      end
-
-      default_task :lint
-
-      def self.source_root
-        File.expand_path("../../../seeds", File.dirname(__FILE__))
       end
 
       private
 
       def bootloader
-        Mugatu::Bootloader.new(root_path: destination_root, registry: registry)
+        Mugatu::Bootloader.new(root_path: current_working_directory, registry: registry)
+      end
+
+      def current_working_directory
+        File.expand_path("")
       end
 
       def registry
