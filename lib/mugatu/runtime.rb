20b181a50a13cbb5e11a67d511731b39c348f669
Zach Ahn <engineering+handy@zachahn.com>
2016-06-14 10:28:45 -0400

    refactor Mugatu::Runtime

--- lib/mugatu/runtime.rb
+++ lib/mugatu/runtime.rb
@@ -1,31 +1,37 @@
 module Mugatu
   class Runtime
-    def initialize(bootloader:, requested_files:, ref: nil, options:)
+    def initialize(bootloader:, requested_files:, ref:, options:)
       @bootloader = bootloader
-
-      @ref =
-        if ref
-          ref
-        else
-          Todd::HashHelpers.dig(bootloader.config, "git", "base", "ref") || "HEAD"
-        end
-
-      @files =
-        if requested_files.empty?
-          Mugatu::Changesets::ChangesetSinceRef.new(bootloader.root_path, @ref)
-        else
-          Mugatu::Changesets::NullChangeset.new(files: requested_files)
-        end
-
-      @formatter =
-        case options[:format]
-        when :json
-          Mugatu::Formatters::Json
-        else
-          Mugatu::Formatters::Pretty
-        end
+      @requested_files = requested_files
+      @ref = ref
+      @options = options
     end
 
-    attr_reader :files, :ref, :formatter
+    attr_reader :formatter
+
+    def ref
+      if @ref
+        @ref
+      else
+        Todd::HashHelpers.dig(@bootloader.config, "git", "base", "ref") || "HEAD"
+      end
+    end
+
+    def files
+      if @requested_files.empty?
+        Mugatu::Changesets::ChangesetSinceRef.new(@bootloader.root_path, ref)
+      else
+        Mugatu::Changesets::NullChangeset.new(files: @requested_files)
+      end
+    end
+
+    def formatter
+      case @options[:format]
+      when :json
+        Mugatu::Formatters::Json
+      else
+        Mugatu::Formatters::Pretty
+      end
+    end
   end
 end
