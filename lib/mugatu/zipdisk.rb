b691e70668267b5c52e980d8f6d5cfa70d6498cb
Zach Ahn <engineering+handy@zachahn.com>
2016-06-15 14:53:13 -0400

    Create a logger

---
+++ lib/mugatu/zipdisk.rb
@@ -0,0 +1,47 @@
+require "logger"
+
+module Mugatu
+  class Zipdisk
+    class NullLogger
+      def initialize(*)
+      end
+
+      def method_missing(*)
+      end
+
+      def respond_to_missing?(*)
+        true
+      end
+    end
+
+    class << self
+      def setup(loglevel)
+        @logger_class =
+          if loglevel.nil? || loglevel == :none
+            NullLogger
+          else
+            ::Logger
+          end
+      end
+
+      def instance
+        @logger ||= @logger_class.new(STDERR)
+      end
+
+      def loglevel=(loglevel)
+        return if loglevel == :none || loglevel.nil?
+
+        loglevel = loglevel.to_s.upcase
+        $logger.level = ::Logger.const_get(loglevel)
+      end
+
+      def method_missing(*args)
+        instance.send(*args)
+      end
+
+      def respond_to_missing?(method_name, include_private = false)
+        instance.respond_to?(method_name, include_private) || super
+      end
+    end
+  end
+end
