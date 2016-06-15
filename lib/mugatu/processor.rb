fe81cb5f10927bff5bd1690eeb784aba58c97fb1
Zach Ahn <engineering+handy@zachahn.com>
2016-06-15 15:44:44 -0400

    Run linter only if there are files to run it on

--- lib/mugatu/processor.rb
+++ lib/mugatu/processor.rb
@@ -28,11 +28,19 @@ module Mugatu
     def problems
       runner = @driver::Runner.new(root: @root)
 
-      result = runner.call(@files)
-      result_hash = normalize_to_hash(result)
+      if pertinent_files.any?
+        Mugatu::Zipdisk.debug("Driver `#{@driver.name}` running with files: #{pertinent_files.inspect}")
 
-      parser = @driver::Parser.new
-      parser.call(result_hash)
+        result = runner.call(pertinent_files)
+        result_hash = normalize_to_hash(result)
+
+        parser = @driver::Parser.new
+        parser.call(result_hash)
+      else
+        Mugatu::Zipdisk.debug("Driver `#{@driver.name}` skipped due to no files")
+
+        []
+      end
     end
 
     def normalize_to_hash(object)
