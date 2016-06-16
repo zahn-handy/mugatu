bf2d82276521ae06b72e2b31208b0232a5f8c4b3
Zach Ahn <engineering+handy@zachahn.com>
2016-06-16 18:52:32 -0400

    replace Cocaine gem in a couple places

--- lib/mugatu/changesets/changeset_since_ref.rb
+++ lib/mugatu/changesets/changeset_since_ref.rb
@@ -28,32 +28,32 @@ module Mugatu
 
       def committed_modified_files
         Dir.chdir(@root) do
-          committed_modified_files_cmd.run(ref: @ref).split("\n")
+          committed_modified_files_cmd(ref: @ref).split("\n")
         end
       end
 
       def staged_modified_files
         Dir.chdir(@root) do
-          staged_modified_files_cmd.run(ref: @ref).split("\n")
+          staged_modified_files_cmd(ref: @ref).split("\n")
         end
       end
 
       def unstaged_files
         Dir.chdir(@root) do
-          unstaged_files_cmd.run.split("\n")
+          unstaged_files_cmd.split("\n")
         end
       end
 
-      def committed_modified_files_cmd
-        Cocaine::CommandLine.new("git", "diff --name-only --diff-filter=AM :ref")
+      def committed_modified_files_cmd(ref:)
+        Todd::System.call("git", "diff", "--name-only", "--diff-filter=AM", ref)
       end
 
-      def staged_modified_files_cmd
-        Cocaine::CommandLine.new("git", "diff-index --name-only --diff-filter=AM :ref")
+      def staged_modified_files_cmd(ref:)
+        Todd::System.call("git", "diff-index", "--name-only", "--diff-filter=AM", ref)
       end
 
       def unstaged_files_cmd
-        Cocaine::CommandLine.new("git", "ls-files --others --exclude-standard")
+        Todd::System.call("git", "ls-files", "--others", "--exclude-standard")
       end
     end
   end
