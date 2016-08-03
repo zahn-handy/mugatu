2307cbca4db2d8d0c4df88973534e0cf08328de7
Zach Ahn <engineering+handy@zachahn.com>
2016-08-03 18:37:21 -0400

    cstash!

--- lib/mugatu/cli/main.rb
+++ lib/mugatu/cli/main.rb
@@ -16,34 +16,48 @@ module Mugatu
       end
 
       def call
-        problems = @application.lint(@runtime.files)
+        p @runtime.options
+        p @bootloader.config
+        result = pipe(
+          Mugatu::Pipes::Diff.new(base: @bootloader.config["git"]["base"]["ref"], compare: "HEAD"),
+          Mugatu::Pipes::DiffParser.new,
+        )
 
-        printable_problems =
-          problems.select do |problem|
-            contexts = @additions_hash[problem.file]
+        p result
+        # problems = @application.lint(@runtime.files)
+        #
+        # printable_problems =
+        #   problems.select do |problem|
+        #     contexts = @additions_hash[problem.file]
+        #
+        #     if contexts.nil?
+        #       Mugatu::Zipdisk.debug("No context for file `#{problem.file}` - assuming new file")
+        #       true
+        #     else
+        #       problem_within_diff =
+        #         contexts.any? do |context|
+        #           context.lines.include?(problem.line)
+        #         end
+        #
+        #       if problem_within_diff
+        #         Mugatu::Zipdisk.debug("Problem within diff: #{problem.inspect}")
+        #       else
+        #         Mugatu::Zipdisk.debug("Problem outside diff: #{problem.inspect}")
+        #       end
+        #
+        #       problem_within_diff
+        #     end
+        #   end
+        #
+        # @formatter.start
+        # printable_problems.each { |p| @formatter.found(p) }
+        # @formatter.done
+      end
 
-            if contexts.nil?
-              Mugatu::Zipdisk.debug("No context for file `#{problem.file}` - assuming new file")
-              true
-            else
-              problem_within_diff =
-                contexts.any? do |context|
-                  context.lines.include?(problem.line)
-                end
-
-              if problem_within_diff
-                Mugatu::Zipdisk.debug("Problem within diff: #{problem.inspect}")
-              else
-                Mugatu::Zipdisk.debug("Problem outside diff: #{problem.inspect}")
-              end
-
-              problem_within_diff
-            end
-          end
-
-        @formatter.start
-        printable_problems.each { |p| @formatter.found(p) }
-        @formatter.done
+      def pipe(*blockishes)
+        blockishes.flatten.each_with_object({}) do |blockish, memo|
+          blockish.call(memo)
+        end
       end
     end
   end
