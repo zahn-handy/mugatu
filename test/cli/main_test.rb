5eb6964bc731bebc15a363bb9b983b417258d3d6
Zach Ahn <engineering+handy@zachahn.com>
2016-08-24 16:24:19 -0400

    Fix tests

--- test/cli/main_test.rb
+++ test/cli/main_test.rb
@@ -4,14 +4,25 @@ class CliMainTest < TestCase
   test "integration of `mugatu lint` command" do
     capture_io do
       Dir.chdir root_path do
-        Mugatu::Cli::Main.new(
-          runtime
-        )
+        Mugatu::Cli::Main.new(config, registry)
       end
     end
   end
 
   private
+
+  def config
+    config = Mugatu::Config.new(
+      root_path: root_path,
+      parsed_argv: {},
+      foreign_argv: [],
+      registry: registry
+    )
+  end
+
+  def registry
+    Mugatu::Registry.new
+  end
 
   def bootloader
     Mugatu::Bootloader.new(
